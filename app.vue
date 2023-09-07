97740a89933aa309f3e109e4a4eebda02941d754
Christian Predoianu <christianpredoianu@yahoo.com>
2023-09-07 14:24:25 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-blue-500">dsadsa</h1>
+    <Button />
+  </div>
+</template>
---
+++ components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <button class="bg-red-500 text-red-500">dsadsa</button>
+  </div>
+</template>
