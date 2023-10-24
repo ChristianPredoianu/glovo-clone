646040b4a170039d676e6589460ce8433c2a9900
Christian Predoianu <christianpredoianu@yahoo.com>
2023-10-24 12:17:01 +0200

    fixed test

--- components/header/appHeader.test.js
+++ components/header/appHeader.test.js
@@ -1,22 +1,25 @@
 import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 
-import AppHeader from '../header/AppHeader.vue'; // Adjust the path
+import AppHeader from '../header/AppHeader.vue';
 
 describe('AppHeader', () => {
   it('should toggle navigation when the button is clicked', async () => {
     const wrapper = mount(AppHeader);
 
-    expect(wrapper.classes()).toContain('-translate-x-[50rem]');
-    /* 
+    expect(wrapper.vm.isNavOpen).toBe(false);
+
     const button = wrapper.find('button');
+    await button.trigger('click');
+
+    expect(wrapper.vm.isNavOpen).toBe(true);
+
+    const navListDiv = wrapper.find('.nav-list-div');
+    expect(navListDiv.classes()).toContain('translate-x-[0rem]');
 
     await button.trigger('click');
 
-    expect(wrapper.classes()).toContain('translate-x-[0rem]'); */
-
-    /*     await button.trigger('click');
-
-    expect(wrapper.classes()).not.toContain('-translate-x-[50rem]'); */
+    expect(wrapper.vm.isNavOpen).toBe(false);
+    expect(navListDiv.classes()).toContain('-translate-x-[50rem]');
   });
 });
