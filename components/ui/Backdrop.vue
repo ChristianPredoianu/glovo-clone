84156c9588a5b3ca3056535901aa25148cad9126
Christian Predoianu <christianpredoianu@yahoo.com>
2023-10-24 13:14:31 +0200

    added ui folder

---
+++ components/ui/Backdrop.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isBackdropOpen = ref(false);
+</script>
+
+<template>
+  <div
+    class="absolute inset-0 min-h-screen w-full bg-slate-200 z-50 opacity-50"
+    v-if="isBackdropOpen"
+  ></div>
+</template>
